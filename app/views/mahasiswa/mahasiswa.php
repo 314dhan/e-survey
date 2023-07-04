b5cc8036bbb4e78243d5b4c6aef001070268dc6d
ramadhan <ramadhan25321@gmail.com>
2023-07-04 09:43:44 +0700

    berhasil menambahkan pop up untuk mengirim survey dengan punya mahasiswa juga

--- app/views/mahasiswa/mahasiswa.php
+++ app/views/mahasiswa/mahasiswa.php
@@ -6,9 +6,9 @@ ini_set('display_errors', 1);
 require "../../config/connection.php";
 
 // Verifikasi cookie
-if(isset($_COOKIE['user_role'])){
+if (isset($_COOKIE['user_role'])) {
     $user_role = $_COOKIE['user_role'];
-    
+
     if ($user_role === 'mahasiswa') {
         header("Location: ../views/mahasiswa/mahasiswa.php");
         exit;
@@ -18,7 +18,7 @@ if(isset($_COOKIE['user_role'])){
     }
 }
 // Cek apakah pengguna sudah login menggunakan session
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     // Jika pengguna belum login, redirect ke halaman login
     header("Location: login.php");
     exit;
@@ -34,10 +34,11 @@ require "../navbar.php";
 ?>
 <div class="container">
 
-    <h1 style="text-align: center;">Selamat datang, <?= $nama; ?>!</h4></h1>
+    <h1 style="text-align: center;">Selamat datang, <?= $nama; ?>!</h4>
+    </h1>
 
     <h3>Pertanyaan Survei</h3>
-<form action="../../controller/surveyControllerMhs.php" method="post">
+    <form action="../../controller/surveyControllerMhs.php" method="post">
         <div class="container" style="text-align: center;">
             <table class="table table-bordered table-striped">
                 <thead class="thead-dark">
@@ -50,69 +51,131 @@ require "../navbar.php";
                     </tr>
                 </thead>
                 <tr>
-                <input type="hidden" name="nama" value="<?= $nama ?>">
+                    <input type="hidden" name="nama" value="<?= $nama ?>">
                 </tr>
                 <tr>
                     <td>1</td>
-                    <td><p>Seberapa puas Anda dengan fasilitas kampus?</p></td>
+                    <td>
+                        <p>Seberapa puas Anda dengan fasilitas kampus?</p>
+                    </td>
                     <td><input type="radio" name="jawaban1" value="1"></td>
                     <td><input type="radio" name="jawaban1" value="2"></td>
                     <td><input type="radio" name="jawaban1" value="3"></td>
                 </tr>
                 <tr>
                     <td>2</td>
-                    <td><p>Bagaimana penilaian Anda terhadap kualitas pengajaran yang diberikan oleh dosen di kampus ini?</p></td>
+                    <td>
+                        <p>Bagaimana penilaian Anda terhadap kualitas pengajaran yang diberikan oleh dosen di kampus ini?</p>
+                    </td>
                     <td><input type="radio" name="jawaban2" value="1"></td>
                     <td><input type="radio" name="jawaban2" value="2"></td>
                     <td><input type="radio" name="jawaban2" value="3"></td>
                 </tr>
                 <tr>
                     <td>3</td>
-                    <td><p>Sejauh mana kampus ini menyediakan fasilitas yang memadai untuk mendukung kegiatan belajar-mengajar?</p></td>
+                    <td>
+                        <p>Sejauh mana kampus ini menyediakan fasilitas yang memadai untuk mendukung kegiatan belajar-mengajar?</p>
+                    </td>
                     <td><input type="radio" name="jawaban3" value="1"></td>
                     <td><input type="radio" name="jawaban3" value="2"></td>
                     <td><input type="radio" name="jawaban3" value="3"></td>
                 </tr>
                 <tr>
                     <td>4</td>
-                    <td><p>Bagaimana tingkat kepuasan Anda terhadap layanan administrasi dan pelayanan mahasiswa di kampus ini?</p></td>
+                    <td>
+                        <p>Bagaimana tingkat kepuasan Anda terhadap layanan administrasi dan pelayanan mahasiswa di kampus ini?</p>
+                    </td>
                     <td><input type="radio" name="jawaban4" value="1"></td>
                     <td><input type="radio" name="jawaban4" value="2"></td>
                     <td><input type="radio" name="jawaban4" value="3"></td>
                 </tr>
                 <tr>
                     <td>5</td>
-                    <td><p>Seberapa efektif sistem penilaian dan evaluasi di kampus ini dalam mengukur kemajuan dan pencapaian mahasiswa?</p></td>
+                    <td>
+                        <p>Seberapa efektif sistem penilaian dan evaluasi di kampus ini dalam mengukur kemajuan dan pencapaian mahasiswa?</p>
+                    </td>
                     <td><input type="radio" name="jawaban5" value="1"></td>
                     <td><input type="radio" name="jawaban5" value="2"></td>
                     <td><input type="radio" name="jawaban5" value="3"></td>
                 </tr>
                 <tr>
                     <td>6</td>
-                    <td><p>Seberapa baik kampus ini memberikan kesempatan bagi mahasiswa untuk mengembangkan potensi dan minat di luar kegiatan akademik?</p></td>
+                    <td>
+                        <p>Seberapa baik kampus ini memberikan kesempatan bagi mahasiswa untuk mengembangkan potensi dan minat di luar kegiatan akademik?</p>
+                    </td>
                     <td><input type="radio" name="jawaban6" value="1"></td>
                     <td><input type="radio" name="jawaban6" value="2"></td>
                     <td><input type="radio" name="jawaban6" value="3"></td>
                 </tr>
                 <tr>
                     <td>7</td>
-                    <td><p>Bagaimana penilaian Anda terhadap kualitas fasilitas perpustakaan yang tersedia di kampus ini?</p></td>
+                    <td>
+                        <p>Bagaimana penilaian Anda terhadap kualitas fasilitas perpustakaan yang tersedia di kampus ini?</p>
+                    </td>
                     <td><input type="radio" name="jawaban7" value="1"></td>
                     <td><input type="radio" name="jawaban7" value="2"></td>
                     <td><input type="radio" name="jawaban7" value="3"></td>
                 </tr>
                 <tr>
                     <td>8</td>
-                    <td><p>Seberapa efektif kampus ini dalam memberikan dukungan dan bimbingan karier bagi mahasiswa?</p></td>
+                    <td>
+                        <p>Seberapa efektif kampus ini dalam memberikan dukungan dan bimbingan karier bagi mahasiswa?</p>
+                    </td>
                     <td><input type="radio" name="jawaban8" value="1"></td>
                     <td><input type="radio" name="jawaban8" value="2"></td>
                     <td><input type="radio" name="jawaban8" value="3"></td>
                 </tr>
             </table>
         </div>
-            <input class="btn btn-success" type="submit" name="submit" value="Kirim Survei">
-</form>
+        <input class="btn btn-success" type="submit" name="submit" value="Kirim Survei">
+    </form>
 </div>
+<script>
+        $(document).ready(function() {
+            $('form').submit(function(event) {
+                event.preventDefault(); // Mencegah pengiriman form
+
+                // Dapatkan data form
+                var formData = $(this).serialize();
+
+                // Kirim permintaan AJAX
+                $.ajax({
+                    url: '../../controller/surveyControllerMhs.php',
+                    type: 'POST',
+                    data: formData,
+                    dataType: 'json',
+                    success: function(response) {
+                        if (response.status === 'success') {
+                            // Tampilkan pesan keberhasilan menggunakan SweetAlert
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil',
+                                text: 'Survey Berhasil Terkirim, Terima Kasih!',
+                            })
+                            // .then(function() {
+                            //     // Redirect ke halaman dosen.php setelah menekan tombol OK pada SweetAlert
+                            //     window.location.href = '../dosen/dosen.php';
+                            // });
+                        } else {
+                            // Tampilkan pesan kesalahan jika terjadi error
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: response.message,
+                            });
+                        }
+                    },
+                    error: function() {
+                        // Tampilkan pesan kesalahan jika permintaan gagal
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Terjadi kesalahan!',
+                        });
+                    }
+                });
+            });
+        });
+</script>
 <?php
 require "../footer.php";
-
