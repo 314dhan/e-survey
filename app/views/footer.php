ecd68375954ca5103c78fadc3dc76fe08079bfff
ramadhan <ramadhan25321@gmail.com>
2023-07-10 09:37:21 +0700

    menambahkan perubahan

--- app/views/footer.php
+++ app/views/footer.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row mt-3">
             <div class="col text-center mt-3">
-                <p>&copy; <?php echo date("Y"); ?> Survey Universitas. All rights reserved.</p>
+                <p>&copy; <?php echo date("Y"); ?> Survey Universitas Serang Raya. All rights reserved.</p>
             </div>
         </div>
     </div>
